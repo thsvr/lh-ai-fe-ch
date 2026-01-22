99dfd027b6958520ae2147b49ef51eb00fd6fb30
thais-cf <thais@connect-financial>
2026-01-22 09:00:40 -0300

    chore: configure path aliases in vite and tsconfig

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@constants': path.resolve(__dirname, './src/constants'),
+      '@data': path.resolve(__dirname, './src/data'),
+      '@designSystem': path.resolve(__dirname, './src/designSystem'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@utils': path.resolve(__dirname, './src/utils'),
+    },
+  },
 })
